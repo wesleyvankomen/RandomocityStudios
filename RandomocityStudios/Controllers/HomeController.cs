76339a4ec0d995a86dbdd52b6618dc734e42bc06
Wesley <WesleyVanKomen@gmail.com>
2019-07-08 20:33:23 -0700

    initial commit for repo

---
+++ RandomocityStudios/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using RandomocityStudios.Models;
+
+namespace RandomocityStudios.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public HomeController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        /// <summary>
+        /// Returns a splash page for incoming users
+        /// </summary>
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        /// <summary>
+        /// Returns a page with a list of projects
+        /// </summary>
+        public IActionResult Projects()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        /// <summary>
+        /// Returns a pdf resume file stream
+        /// </summary>
+        public IActionResult Resume()
+        {
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            var stream = new FileStream(webRootPath+@"\files\test.pdf", FileMode.Open);
+            return new FileStreamResult(stream, "application/pdf");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
