e901273f1aaec543ffd3cf99fa1b30da82948689
Wesley <wesleyvankomen@gmail.com>
2019-07-09 22:43:22 -0700

    add project model and add more placeholder styling

---
+++ RandomocityStudios/Models/Project.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RandomocityStudios.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public string Descriptions { get; set; }
+    }
+}
