5037d8c0c511d410440ecffa7c271e2328111d38
Wesley <wesleyvankomen@gmail.com>
2019-08-17 17:43:53 -0700

    layour changes, add animated favicon, and setup bundling

--- RandomocityStudios/wwwroot/js/site.js
+++ RandomocityStudios/wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const rootPath = '/images/favicon/sun_favicon/sun_favicon_';
+const imageCount = 10;
+var currentImage = 1;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+sleep(2000);
+
+setInterval(function () {
+    $("link[rel='icon']").remove();
+    $("link[rel='shortcut icon']").remove();
+    $("head").append('<link rel="icon" href="' + rootPath + currentImage + '.png' + '" type="image/png">');
+
+    // If last image then goto first image
+    // Else go to next image    
+    if (currentImage == imageCount)
+        currentImage = 1;
+    else
+        currentImage++;
+}, 1000);
+
