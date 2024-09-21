38b4d9d443b8e28f189c458fc1fad11aa5ac32f6
Wesley <Wesleyvankomen@gmail.com>
2024-09-20 17:01:50 -0700

    Add an lazyLoader replacement to bulk load images after page load

---
+++ RandomocityStudios/wwwroot/js/imageLoader.js
@@ -0,0 +1,9 @@
+$(function () {
+
+    var images = $('.lazy-load').find('img[data-src]');
+    images.each(function () {
+        var $this = $(this);
+        $this.attr('src', $this.attr('data-src'));
+        $this.removeAttr('data-src');
+    });
+});
