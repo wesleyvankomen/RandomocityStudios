2366efb344eec8a24e51c2baecd8f6a3a3d60986
Wesley <Wesleyvankomen@gmail.com>
2024-09-20 17:41:02 -0700

    Resolve remaining compiler warnings

--- RandomocityStudios/wwwroot/js/YouTubePlayerController.js
+++ RandomocityStudios/wwwroot/js/YouTubePlayerController.js
@@ -2,6 +2,6 @@ function stopVideo() {
     this.contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
 }
 
-$('#musicLeft').add('#musicRight').add(".carousel-selector-pull-down").click(function () {
+$('#musicLeft, #musicRight, .carousel-selector-pull-down').on('click', function () {
     $('.yt_player_iframe').each(stopVideo);
 });
